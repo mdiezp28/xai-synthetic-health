b797bc0ce1571b0d18e7c2cc04cf1a3dafaecb20
Maria Diez <m.diezperez@student.maastrichtuniversity.nl>
2026-02-18 17:37:12 +0100

    feat: find best hyperparameters xgboost

--- syn_data_evaluation/models/xgboost.py
+++ syn_data_evaluation/models/xgboost.py
@@ -1,4 +1,5 @@
-from sklearn.model_selection import GridSearchCV
+from matplotlib.pyplot import grid
+from sklearn.model_selection import GridSearchCV, PredefinedSplit
 import xgboost as xgb
 
 class XGBoostModel:
@@ -10,39 +11,62 @@ class XGBoostModel:
     def get_scale_pos_weight(y_train):
         scale_pos_weight = len(y_train[y_train == 0]) / len(y_train[y_train == 1])
         return scale_pos_weight
-    
-    @staticmethod
-    def find_best_params(x_train, y_train):
+
+
+    def find_best_params(self, x_train, y_train, test_fold):
+        scale_pos_weight = self.get_scale_pos_weight(y_train[test_fold == -1])
         param_grid = {
-            'n_estimators': [100],
-            'max_depth': [3],
-            'eta': [0.1],
-            'gamma': [0.25, 0.17],
-            'colsample_bytree': [0.8, 1.0],
-            'min_child_weight': [1],
-            'subsample': [0.5, 0.6],
-            'scale_pos_weight': [10,15,17,20],
+            'n_estimators': [100, 150],
+            'max_depth': [3, 4],
+            'gamma': [0, 0.25],
+            'colsample_bytree': [ 0.7],
+            'subsample': [0.5],
+            'reg_alpha': [0.1, 0.3],
+            'reg_lambda': [1.0, 2.0],
+            'max_delta_step': [0],
         }
-        grid = GridSearchCV(xgb.XGBClassifier(eval_metric='auc'), param_grid, scoring='recall', cv=5)
+        grid = GridSearchCV(
+            estimator= xgb.XGBClassifier(
+                learning_rate=0.05,
+                min_child_weight=1,
+                eval_metric='aucpr',
+                scale_pos_weight=scale_pos_weight,
+                # random_state=42,
+                # tree_method='hist',  # Use histogram-based algorithm for faster training
+            ), 
+            param_grid=param_grid,
+            scoring='recall',
+            cv=PredefinedSplit(test_fold),
+            n_jobs=-1,
+            refit=False,
+            verbose=2,
+        )
+
         grid.fit(x_train, y_train)
-        print("Best params:", grid.best_params_)
-        print("Best recall (sensitivity):", grid.best_score_)
+
+        best_params = grid.best_params_
+        best_score = grid.best_score_
+
+        print("Best params:", best_params)
+        print(f"Best mean CV score: {best_score}")
+
+        return best_params, best_score, grid
 
 
     def train(self, x_train, y_train):
         self.model = xgb.XGBClassifier(
-            n_estimators=150,
-            max_depth=4, # paper 3 [3, 4]
-            learning_rate=0.1,
-            gamma=0.1, # paper 0.25 [0, 0.1]
+            n_estimators=100,
+            max_depth=3, # paper 3 [3, 4]
+            learning_rate=0.05,
+            gamma=0, # paper 0.25 [0, 0.1]
             colsample_bytree=0.7,
-            min_child_weight=5,
-            subsample=0.7, # paper 0.5 [0.6, 0.8]
+            min_child_weight=1,
+            subsample=0.5, # paper 0.5 [0.6, 0.8]
             scale_pos_weight=self.get_scale_pos_weight(y_train),
-            eval_metric='auc',
-            max_delta_step=1,
-            reg_alpha=0.3,
-            reg_lambda=2.0,
+            eval_metric='aucpr',
+            max_delta_step=0,
+            reg_alpha=0.1,
+            reg_lambda=1.0,
             # colsample_bytree=0.7,  # Only use 50% of features per tree
             # colsample_bylevel=0.8,  # Additional sampling at each level
         )
